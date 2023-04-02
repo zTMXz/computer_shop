3453a2e7c59f32947104c8586a840f535686efac
zTMXz <erekhovets200226@gmail.com>
2023-04-02 23:14:15 +0300

    first commit

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from users.views import Register, profile
+
+urlpatterns = [
+    path("", include('django.contrib.auth.urls')),
+    path("register/", Register.as_view(), name='register'),
+    path("profile/", profile, name='profile'),
+]
