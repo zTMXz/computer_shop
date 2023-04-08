6aa4d1f8ca4952f2b946aa75c370435ae469557e
zTMXz <erekhovets200226@gmail.com>
2023-04-09 00:56:53 +0300

    add cart functional

--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from coupons.forms import CouponApplyForm
 
 
 @require_POST
@@ -27,4 +28,12 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
-    return render(request, template_name='cart/detail.html', context={'cart': cart})
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+                            initial={'quantity': item['quantity'],
+                            'update': True})
+    coupon_apply_form = CouponApplyForm()
+    return render(request,
+                  'cart/detail.html',
+                  {'cart': cart,
+                   'coupon_apply_form': coupon_apply_form})
