378c4061acb4a6969b8d1538c0b89b14e7e00442
zTMXz <erekhovets200226@gmail.com>
2023-04-13 00:54:51 +0300

    upd urls

--- shop/urls.py
+++ shop/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('search/', views.Search.as_view(), name='search'),
     path('<slug:slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
-
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from users.views import Register, profile, send_mail_to_Egor
+from users.views import Register, profile
 
 urlpatterns = [
     path("", include('django.contrib.auth.urls')),
     path("register/", Register.as_view(), name='register'),
     path("profile/", profile, name='profile'),
-    path("egor/", send_mail_to_Egor, name='egor'),
 
 ]
