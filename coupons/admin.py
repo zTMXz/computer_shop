f541eea49a596d727aa5cc27b903dd6d191eae62
zTMXz <erekhovets200226@gmail.com>
2023-04-13 00:54:09 +0300

    add coupons realisation

--- coupons/admin.py
+++ coupons/admin.py
@@ -5,5 +5,6 @@ from .models import Coupon
 class CouponAdmin(admin.ModelAdmin):
     list_display = ['code', 'valid_from', 'valid_to', 'discount', 'active']
     list_filter = ['active', 'valid_from', 'valid_to']
+    list_editable = ['active', 'discount', 'valid_to']
     search_fields = ['code']
 admin.site.register(Coupon, CouponAdmin)
