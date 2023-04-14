18c9d8c20ff8416a26e61e523d37c7d7b03a84b2
zTMXz <erekhovets200226@gmail.com>
2023-04-15 00:14:54 +0300

    add new translations

--- shop/translation.py
+++ shop/translation.py
@@ -1,10 +1,12 @@
 from modeltranslation.translator import register, TranslationOptions
 from .models import Product, PhoneDetails
 
-@register(Product)
-class ProductTranslationOptions(TranslationOptions):
-    fields = ('ph_color_name')
 
 @register(PhoneDetails)
 class PhoneDetailsTranslationOptions(TranslationOptions):
     fields = ('display', 'processor', 'size_and_weight', 'camera', 'video', 'front_cam', 'mobile_cnct')
+
+
+@register(Product)
+class ProductTranslationOptions(TranslationOptions):
+    fields = ('ph_color_name',)
